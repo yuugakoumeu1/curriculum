435ca347de84eea67ceb395c4853d9f62f4558a2
yuugakoumeu1 <77602902+yuugakoumeu1@users.noreply.github.com>
2021-10-02 23:45:20 +0900

    課題提出テスト
    Task1_2

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,38 @@
+/**
+ * Task1-2 : 課題内容
+ *
+ * 本課題では、配列の使い方を学んでいきましょう。
+ * 問①から問⑥まであります。
+ * 指定された値と変数名を守って記述してください。
+ *
+ * 問⑤,⑥については、コメントを入れてください。
+ *
+ */
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+        String[] country = {"JAPAN","AMERICA","KOREA"};
+        // ② ①で作成した配列の要素数を出力してください。
+        System.out.println(country.length);
+        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+         *   1番目（先頭）の要素に 「りんご」 を代入
+         *   2番目の要素に 「もも」 を代入
+         *   3番目の要素に 「ぶどう」 を代入
+         */
+         String[] strArray = {"りんご","もも","ぶどう"};
+
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+        System.out.println(strArray[1]);
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  [int型の配列のintArrayに10,20,30,40,50で初期化]
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // [intArray配列の20+50を足す]
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
